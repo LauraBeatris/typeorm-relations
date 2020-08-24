a99afc4a84e6b61cfd3dd31bfc0e1a7022b96d97
Laura <laurabeatriserafim@gmail.com>
2020-08-24 08:19:51 -0300

    refactor: improve create customer service

--- src/modules/customers/services/CreateCustomerService.ts
+++ src/modules/customers/services/CreateCustomerService.ts
@@ -12,10 +12,25 @@ interface IRequest {
 
 @injectable()
 class CreateCustomerService {
-  constructor(private customersRepository: ICustomersRepository) {}
+  constructor(
+    @inject('CustomersRepository')
+    private customersRepository: ICustomersRepository,
+  ) {}
 
   public async execute({ name, email }: IRequest): Promise<Customer> {
-    // TODO
+    const findCustomerWithSameEmail = await this.customersRepository.findByEmail(
+      email,
+    );
+
+    if (findCustomerWithSameEmail) {
+      throw new AppError(
+        "There's already a customer registered with that email",
+      );
+    }
+
+    const customer = await this.customersRepository.create({ name, email });
+
+    return customer;
   }
 }
 
