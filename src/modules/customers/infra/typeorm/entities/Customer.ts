bcc0f10658a12115c6007e11fcc1ece819b23ebc
Laura <laurabeatriserafim@gmail.com>
2020-08-24 08:05:58 -0300

    chore: connect customer class to typeorm

--- src/modules/customers/infra/typeorm/entities/Customer.ts
+++ src/modules/customers/infra/typeorm/entities/Customer.ts
@@ -6,15 +6,22 @@ import {
   UpdateDateColumn,
 } from 'typeorm';
 
+@Entity('customers')
 class Customer {
+  @Column('uuid')
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   name: string;
 
+  @Column()
   email: string;
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
