9d15e2596ccd9be0056ccce887c3a7108ba83b0e
Laura <laurabeatriserafim@gmail.com>
2020-08-24 08:20:02 -0300

    feat: create customer

--- src/modules/customers/infra/http/controller/CustomersController.ts
+++ src/modules/customers/infra/http/controller/CustomersController.ts
@@ -1,11 +1,28 @@
 import { Request, Response } from 'express';
+import { container } from 'tsyringe';
 
 import CreateCustomerService from '@modules/customers/services/CreateCustomerService';
 
-import { container } from 'tsyringe';
+import AppError from '@shared/errors/AppError';
+import Customer from '@modules/customers/infra/typeorm/entities/Customer';
 
 export default class CustomersController {
-  public async create(request: Request, response: Response): Promise<Response> {
-    // TODO
+  public async create(
+    request: Request,
+    response: Response,
+  ): Promise<Response<Customer>> {
+    const { name, email } = request.body;
+
+    if (!name || !email) {
+      throw new AppError(
+        'Please, provide valid data in order to create a customer',
+      );
+    }
+
+    const createCustomer = container.resolve(CreateCustomerService);
+
+    const customer = await createCustomer.execute({ name, email });
+
+    return response.json(customer);
   }
 }
