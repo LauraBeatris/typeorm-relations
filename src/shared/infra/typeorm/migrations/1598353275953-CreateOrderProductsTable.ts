aa1138c67059e4527d780c42687181e471ecdc22
Laura <laurabeatriserafim@gmail.com>
2020-08-25 08:18:05 -0300

    chore: add order_products table

---
+++ src/shared/infra/typeorm/migrations/1598353275953-CreateOrderProductsTable.ts
@@ -0,0 +1,70 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateOrderProductsTable1598353275953
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'order_products',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+          },
+          {
+            name: 'price',
+            type: 'decimal',
+            precision: 5,
+            scale: 2,
+          },
+          {
+            name: 'quantity',
+            type: 'integer',
+          },
+          {
+            name: 'order_id',
+            type: 'varchar',
+          },
+          {
+            name: 'product_id',
+            type: 'varchar',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+        foreignKeys: [
+          {
+            name: 'orders_order_products_fk',
+            onUpdate: 'CASCADE',
+            onDelete: 'CASCADE',
+            columnNames: ['order_id'],
+            referencedColumnNames: ['id'],
+            referencedTableName: 'orders',
+          },
+          {
+            name: 'order_products_products_fk',
+            onUpdate: 'CASCADE',
+            onDelete: 'CASCADE',
+            columnNames: ['product_id'],
+            referencedColumnNames: ['id'],
+            referencedTableName: 'products',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('order_products');
+  }
+}
