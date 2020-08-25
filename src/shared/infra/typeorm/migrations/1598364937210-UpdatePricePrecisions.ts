ab0711f0dcbeee9b1621bbd2e9ffd73a108e885c
Laura <laurabeatriserafim@gmail.com>
2020-08-25 11:37:38 -0300

    fix: increase precision of the decimal columns

---
+++ src/shared/infra/typeorm/migrations/1598364937210-UpdatePricePrecisions.ts
@@ -0,0 +1,52 @@
+import { MigrationInterface, QueryRunner, TableColumn } from 'typeorm';
+
+export default class UpdatePricePrecisions1598364937210
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.changeColumn(
+      'order_products',
+      'price',
+      new TableColumn({
+        name: 'price',
+        type: 'decimal',
+        precision: 10,
+        scale: 2,
+      }),
+    );
+
+    await queryRunner.changeColumn(
+      'products',
+      'price',
+      new TableColumn({
+        name: 'price',
+        type: 'decimal',
+        precision: 10,
+        scale: 2,
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.changeColumn(
+      'order_products',
+      'price',
+      new TableColumn({
+        name: 'price',
+        type: 'decimal',
+        precision: 5,
+        scale: 2,
+      }),
+    );
+
+    await queryRunner.changeColumn(
+      'products',
+      'price',
+      new TableColumn({
+        name: 'price',
+        type: 'decimal',
+        precision: 5,
+        scale: 2,
+      }),
+    );
+  }
+}
