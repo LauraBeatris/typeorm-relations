784c30bcefaa8b691b7ba5410eb99331a1a901b5
Laura <laurabeatriserafim@gmail.com>
2020-08-25 10:01:06 -0300

    refactor: apply default value to order_products id column

---
+++ src/shared/infra/typeorm/migrations/1598359381489-UpdateOrdersProductsIdColumn.ts
@@ -0,0 +1,31 @@
+import { MigrationInterface, QueryRunner, TableColumn } from 'typeorm';
+
+export default class UpdateOrdersProductsIdColumn1598359381489
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.changeColumn(
+      'order_products',
+      'id',
+      new TableColumn({
+        name: 'id',
+        type: 'uuid',
+        default: 'uuid_generate_v4()',
+        isPrimary: true,
+        generationStrategy: 'uuid',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.changeColumn(
+      'orders_products',
+      'id',
+      new TableColumn({
+        name: 'id',
+        type: 'uuid',
+        isPrimary: true,
+        generationStrategy: 'uuid',
+      }),
+    );
+  }
+}
