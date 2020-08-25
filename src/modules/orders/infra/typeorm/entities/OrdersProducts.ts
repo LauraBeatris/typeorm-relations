4c263cae84962d6cf1a543de145b07b57181d7ae
Laura <laurabeatriserafim@gmail.com>
2020-08-25 08:19:01 -0300

    chore: connect orders products to typeorm

--- src/modules/orders/infra/typeorm/entities/OrdersProducts.ts
+++ src/modules/orders/infra/typeorm/entities/OrdersProducts.ts
@@ -1,33 +1,46 @@
 import {
   Entity,
   Column,
+  ManyToOne,
+  JoinColumn,
   CreateDateColumn,
   UpdateDateColumn,
-  JoinColumn,
   PrimaryGeneratedColumn,
-  ManyToOne,
 } from 'typeorm';
 
 import Order from '@modules/orders/infra/typeorm/entities/Order';
 import Product from '@modules/products/infra/typeorm/entities/Product';
 
+@Entity('order_products')
 class OrdersProducts {
+  @Column('uuid')
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @ManyToOne(() => Order, order => order.order_products)
+  @JoinColumn({ name: 'order_id' })
   order: Order;
 
+  @ManyToOne(() => Product, product => product.order_products)
+  @JoinColumn({ name: 'product_id' })
   product: Product;
 
+  @Column('uuid')
   product_id: string;
 
+  @Column('uuid')
   order_id: string;
 
+  @Column('decimal', { precision: 5, scale: 2 })
   price: number;
 
+  @Column('integer')
   quantity: number;
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
