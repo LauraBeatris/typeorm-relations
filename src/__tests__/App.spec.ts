815ed2a729546bd074ea9034c89e6fbab1478638
Laura <laurabeatriserafim@gmail.com>
2020-09-06 11:03:37 -0300

    fix: update table name

--- src/__tests__/App.spec.ts
+++ src/__tests__/App.spec.ts
@@ -13,7 +13,7 @@ describe('App', () => {
   beforeAll(async () => {
     connection = await createConnection('test-connection');
 
-    await connection.query('DROP TABLE IF EXISTS order_products');
+    await connection.query('DROP TABLE IF EXISTS orders_products');
     await connection.query('DROP TABLE IF EXISTS orders');
     await connection.query('DROP TABLE IF EXISTS products');
     await connection.query('DROP TABLE IF EXISTS customers');
@@ -23,7 +23,7 @@ describe('App', () => {
   });
 
   beforeEach(async () => {
-    await connection.query('DELETE FROM order_products');
+    await connection.query('DELETE FROM orders_products');
     await connection.query('DELETE FROM orders');
     await connection.query('DELETE FROM products');
     await connection.query('DELETE FROM customers');
